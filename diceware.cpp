7e93b0b3fe00d2eb99947272b060f554f0653bc7
typedeph <alexegoico@gmail.com>
2016-04-25 16:18:52 -0400

    Refactored out boost and simplifying the code

--- diceware.cpp
+++ diceware.cpp
@@ -1,20 +1,28 @@
 #include <fstream>
+#include <iostream>
 #include <string>
-#include <cmath>
 #include <random>
 #include <unordered_map>
 #include <vector>
 #include <stdexcept>
 
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
-using boost::numeric::ublas::matrix;
+struct FileNotFound : public std::runtime_error
+{
+  explicit FileNotFound(const char* message)
+    : std::runtime_error{message} { };
+  explicit FileNotFound(const std::string& message)
+    : std::runtime_error{message.c_str()} { };
+};
 
-void read_dict(std::unordered_map<int, std::string>& map, std::string filename)
+void read_dict(const std::string& filename,
+               std::unordered_map<int, std::string>& map)
 {
   int key;
   std::string word;
+
   std::fstream fs{filename, std::fstream::in};
+  if (fs.fail())
+    throw FileNotFound{filename + " is not in the current directory"};
 
   while (!fs.eof()) {
     fs >> key;
@@ -24,55 +32,51 @@ void read_dict(std::unordered_map<int, std::string>& map, std::string filename)
   }
 }
 
-void roll_dice_sets(matrix<int>& mat)
+// Fills the rows with digits
+std::vector<int> get_word_codes(int no_of_words, int digits_per_word)
 {
-  std::random_device rd;  // non-deterministic entropy.
+  std::vector<int> temp(no_of_words, 0);
+
+  std::random_device rd;
   std::mt19937 mt{rd()};
   std::uniform_int_distribution<int> dist(1, 5);
-  for (int i = 0; i < mat.size1(); ++i)
-    for (int j = 0; j < mat.size2(); ++j)
-      mat(i, j) = dist(mt);
-}
+  for (int i = 0; i < no_of_words; ++i) {
+    for (int j = digits_per_word-1; j >= 0; --j)
+      temp[i] += dist(mt) * pow(10, j);
+  }
 
-std::vector<int> bit_matrix_to_nums(matrix<int>& mat)
-{
-  std::vector<int> temp(mat.size1(), 0);
-  for (int i = 0; i < mat.size1(); ++i)
-    for (int j = 0; j < mat.size2(); ++j)
-      temp[i] += mat(i, j) * pow(10, j); // not all that portable, but I can trust ints to have 5 bits.
-
-  return temp;
+  return std::move(temp);
 }
 
 int main(int argc, char* argv[])
 try
 {
-  if (argc != 2)
-    throw std::invalid_argument{"Usage: "
-        "./diceware <no_of_words>"};
+  if (argc != 2) {
+    std::cout << "Usage: ./diceware <no_of_words>\n";
+    return -3;
+  }
   
   std::unordered_map<int, std::string> dice_map;
-  read_dict(dice_map, "diceware_dict.txt");
+  read_dict("diceware_dict.txt", dice_map);
     
+  // Creates a matrix of
+  // words in pass x digits_per_word
   const     int no_of_words = std::stoi(argv[1]);
-  constexpr int digits_per_word = 5;  // compile time #define, way better than #define
-  matrix<int> m{no_of_words, digits_per_word};
-  roll_dice_sets(m);
-
-  std::vector<int> word_codes = bit_matrix_to_nums(m);
+  constexpr int digits_per_word = 5;
+  const std::vector<int> word_codes = get_word_codes(no_of_words, digits_per_word);
 
   std::cout << "Here is your diceware word list\n";
+  std::cout << "-------------------------------\n";
   for (int code : word_codes)
     std::cout << dice_map[code] << " ";
   std::cout << "\n";
 }
-catch (std::invalid_argument e)
-{
-  e.what();
-  return -3;
+catch (const FileNotFound& e) {
+  std::cout << e.what();
+  return -4;
 }
 catch (...)
 {
   std::cout << "Something strange happened...";
-  return -10;
+  return -77;
 }
